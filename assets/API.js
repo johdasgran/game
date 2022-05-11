f6f39af860d92d396c5e1099afaea0eafa27e1c2
Johdasgran <dampig14@gmail.com>
2022-05-10 21:37:25 -0500

    add random

--- assets/API.js
+++ assets/API.js
@@ -4,4 +4,13 @@ fetch("https://finalspaceapi.com/api/v0/character")
     console.log(datos[0].name)
 })
 
-g
+
+min = 1
+max = 20
+console.log(Math.floor(Math.random() * (max - min) ) + min);
+
+// Juego de memoria, recordar imagenes de personajes con la API Final Space,
+
+// Jugar con animaciones al inico e iteractividad para el jugador xd
+
+// GUardar puntaje en localStorage y recordar nombre

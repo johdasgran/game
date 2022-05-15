c7b309c3e450065821eb6906222addcb87bba794
Johdasgran <dampig14@gmail.com>
2022-05-15 01:47:11 -0500

    Add images

--- assets/API.js
+++ assets/API.js
@@ -1,7 +1,26 @@
 fetch("https://finalspaceapi.com/api/v0/character")
 .then(resp => resp.json())
 .then(datos => {
-    console.log(datos[0].name)
+    console.log(datos);
+    const cardsImage = datos;
+    cardsImage.forEach(element => {
+        console.log(element.name);
+
+        let container = document.querySelector("main.main");
+    
+         // Container image
+         let card_image = document.createElement("div");
+         card_image.id = "image";
+         container.appendChild(card_image);
+   
+         // Image
+         let image = document.createElement("img");
+         image.src = element.img_url;
+         card_image.appendChild(image);
+    });
+    
+
+
 })
 
 
@@ -9,8 +28,8 @@ min = 1
 max = 20
 console.log(Math.floor(Math.random() * (max - min) ) + min);
 
+
+
 // Juego de memoria, recordar imagenes de personajes con la API Final Space,
-
 // Jugar con animaciones al inico e iteractividad para el jugador xd
-
 // GUardar puntaje en localStorage y recordar nombre

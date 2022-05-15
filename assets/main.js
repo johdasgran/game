ad2dba5d7437af9f4061ce5e3ecb581b80e50354
Johdasgran <dampig14@gmail.com>
2022-05-15 17:55:44 -0500

    add data localStorage

---
+++ assets/main.js
@@ -0,0 +1,19 @@
+
+
+
+const nameUser = document.querySelector('#name');
+const addName = document.querySelector('#addName');
+
+const dataUser = localStorage.getItem('game');
+
+console.log(JSON.parse(dataUser))
+
+addName.addEventListener("click", (e)=>{
+    console.log(nameUser.value);
+
+    localStorage.setItem('game', JSON.stringify({
+        'name': nameUser.value,
+        'level': 0
+    }))
+
+})

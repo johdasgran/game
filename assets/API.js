eaa474134a117624260da33d334b910227463815
Johdasgran <dampig14@gmail.com>
2022-05-15 23:00:31 -0500

    horror code xd

--- assets/API.js
+++ assets/API.js
@@ -45,21 +45,45 @@
 
 
 
+
+
+
+
+
+
+
+function desordenar(unArray){
+    var t = unArray.sort(function(a,b) {return (Math.random()-0.5)});
+    return [...t];
+  } // esta función me desordena un array
+  
+
+// xdddddddddddd////////////////////
+
   
 let arrayNumbers = [];
 
 const min = 1;
 const max = 47;
 
-for (let i = 0; arrayNumbers.length < 10; i++) {
+for (let i = 0; arrayNumbers.length < 12; i++) {
   const n = Math.floor(Math.random() * (max - min)) + min;
   !arrayNumbers.includes(n) && arrayNumbers.push(n);
 }
 
+numerosDesordenados = [];
 
-console.log(arrayNumbers);
+for (i=0;i<1;i++){ //creo bucle para llenar array vacío
+    x = desordenar(arrayNumbers);
+    numerosDesordenados[i] = x;
+    // console.log(i,x);
+  }
 
-for (let i = 0; i < 10; i++) {
+  console.log(numerosDesordenados[0])
+
+// console.log(arrayNumbers);
+
+for (let i = 0; i < 12; i++) {
   fetch(`https://finalspaceapi.com/api/v0/character/${arrayNumbers[i]}`)
     .then((resp) => resp.json())
     .then((datos) => {
@@ -79,16 +103,16 @@ for (let i = 0; i < 10; i++) {
       image.classList = "card-image";
       card_image.appendChild(image);
 
-       // Container image
-       let card = document.createElement("div");
-       card.id = "image";
-       container.appendChild(card);
+    //    // Container image
+    //    let card = document.createElement("div");
+    //    card.id = "image";
+    //    container.appendChild(card);
  
-       // Image
-       let imag = document.createElement("img");
-       imag.src = datos.img_url;
-       imag.classList = "card-image";
-       card.appendChild(imag);
+    //    // Image
+    //    let imag = document.createElement("img");
+    //    imag.src = datos.img_url;
+    //    imag.classList = "card-image";
+    //    card.appendChild(imag);
 
     });
 
@@ -99,3 +123,33 @@ for (let i = 0; i < 10; i++) {
 // Juego de memoria, recordar imagenes de personajes con la API Final Space,
 // Jugar con animaciones al inico e iteractividad para el jugador xd
 // GUardar puntaje en localStorage y recordar nombre
+
+
+  
+//   console.log(numerosDesordenados);
+
+
+  for (let i = 0; i < 12; i++) {
+    fetch(`https://finalspaceapi.com/api/v0/character/${numerosDesordenados[0][i]}`)
+      .then((resp) => resp.json())
+      .then((datos) => {
+        console.log(datos);
+        const cardsImage = datos;
+  
+        let container = document.querySelector("div#app");
+  
+        // Container image
+        let card_image = document.createElement("div");
+        card_image.id = "image";
+        container.appendChild(card_image);
+  
+        // Image
+        let image = document.createElement("img");
+        image.src = datos.img_url;
+        image.classList = "card-image";
+        card_image.appendChild(image);
+  
+      });
+  
+  }
+  

ffccfa9fa11f36ca0a5f21a34da138bda835abfa
Johdasgran <dampig14@gmail.com>
2022-05-15 22:05:16 -0500

    add random number

--- assets/API.js
+++ assets/API.js
@@ -1,33 +1,68 @@
 fetch("https://finalspaceapi.com/api/v0/character/?limit=24")
-.then(resp => resp.json())
-.then(datos => {
-    console.log(datos);
+  .then((resp) => resp.json())
+  .then((datos) => {
+    // console.log(datos);
     const cardsImage = datos;
-    cardsImage.forEach(element => {
-        console.log(element.name);
 
-        let container = document.querySelector("div#app");
+    let arrayNumbers = [];
+
+    const min = 1;
+    const max = 47;
     
-         // Container image
-         let card_image = document.createElement("div");
-         card_image.id = "image";
-         container.appendChild(card_image);
-   
-         // Image
-        //  let image = document.createElement("img");
-        //  image.src = element.img_url;
-        //  image.classList = "card-image"
-        //  card_image.appendChild(image);
+    for (let i = 0; arrayNumbers.length < 10; i++) {
+      const n = Math.floor(Math.random() * (max - min)) + min;
+      !arrayNumbers.includes(n) && arrayNumbers.push(n);
+    }
+
+    console.log(arrayNumbers)
+
+
+
+ 
+    cardsImage.forEach((element) => {
+      // console.log(element.name);
+
+      let container = document.querySelector("div#app");
+
+      // Container image
+      let card_image = document.createElement("div");
+      card_image.id = "image";
+      container.appendChild(card_image);
+
+      // Image
+      let image = document.createElement("img");
+      image.src = element.img_url;
+      image.classList = "card-image";
+      card_image.appendChild(image);
     });
-    
+  });
 
 
-})
 
 
-min = 1
-max = 20
-console.log(Math.floor(Math.random() * (max - min) ) + min);
+  
+
+
+// let arrayNumbers = [];
+
+// const min = 1;
+// const max = 47;
+
+// for (let i = 0; arrayNumbers.length < 10; i++) {
+//   const n = Math.floor(Math.random() * (max - min)) + min;
+//   !arrayNumbers.includes(n) && arrayNumbers.push(n);
+// }
+
+
+// console.log(arrayNumbers);
+
+// for (let i = 0; i < 10; i++) {
+//   fetch(`https://finalspaceapi.com/api/v0/character/${arrayNumbers[i]}`)
+//     .then((resp) => resp.json())
+//     .then((datos) => {
+//       console.log(datos);
+//     });
+// }
 
 
 
